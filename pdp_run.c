795e9fac6f8059ab6dc00e59d4dd206da2346e8a
hollyleo <hollyleonana@gmail.com>
2024-02-14 15:19:06 +0500

    split pdp_run in separate file

--- pdp_run.c
+++ pdp_run.c
@@ -1,19 +1,7 @@
 #include "pdp_f.h"
-typedef struct
-{
-    word mask;
-    word opcode;
-    char *name;
-    void (*do_func)(void);
-    char params;
-} Command;
 char XX = 0;
 int registr = 0;
 int byte_status = 0;
-int flag_V = 0; // overflow знакое переполнение
-int flag_Z = 0; // zero
-int flag_N = 0; // negative
-int flag_C = 0; // carry , переполнение
 word check_byte = 0100000;
 Arg get_mr(word w)
 {
@@ -49,7 +37,7 @@ Arg get_mr(word w)
         if (r == 6 && byte_status == 1)
             reg[r] += 1;
         if (r == 7)
-            log_pdp(TRACE, "#%o ", res.val);
+            log_pdp(TRACE, "#%06o ", res.val);
         else
             log_pdp(TRACE, "(R%d)+ ", r);
         break;
@@ -95,9 +83,10 @@ Arg get_mr(word w)
         res.adr = res.adr + x;
         res.val = w_read(res.adr);
         if (r == 7)
-            log_pdp(TRACE, "%o ", res.val);
+            log_pdp(TRACE, "%06o ", res.adr);
         else
             log_pdp(TRACE, "X(R%d) ", r);
+        break;
     case 7:
         word tmp = w_read(pc);
         pc += 2;
@@ -106,201 +95,16 @@ Arg get_mr(word w)
         res.adr = w_read(res.adr);
         res.val = w_read(res.adr);
         if (r == 7)
-            log_pdp(TRACE, " @%o ", res.val);
+            log_pdp(TRACE, " @%06o ", res.val);
         else
             log_pdp(TRACE, "@XR(%d) ", r);
+        break;
     default:
         log_pdp(ERROR, "Mode %d not implemented yet!\n", m);
         exit(1);
         break;
     }
     return res;
-}
-void do_halt()
-{
-    log_pdp(TRACE, "%s", "halt\n");
-    for (int i = 0; i < REG_SIZE; i++)
-    {
-        log_pdp(TRACE, "r%d:%o ", i, reg[i]);
-    }
-    printf("\n");
-    log_pdp(TRACE, "%s\n", "THE END!!!!!!");
-    exit(0);
-}
-void do_mov()
-{
-    word res = ss.val;
-    w_write(dd.adr, ss.val);
-    if (res == 0)
-    {
-        set_flag_z();
-    }
-    else
-        clear_flag_z();
-    if ((res & 0200) == 0200)
-    {
-        set_flag_n();
-    }
-    else
-        clear_flag_n();
-    log_pdp(TRACE, "%s", "mov\n");
-}
-void do_add()
-{
-    word res = ss.val + dd.val;
-    w_write(dd.adr, ss.val + dd.val);
-    if (res == 0)
-    {
-        set_flag_z();
-    }
-    if (((res >> 8) & 0200) == 0200)
-    {
-        set_flag_n();
-    }
-    if (((res >> 8) & 0400) == 0400)
-    {
-        set_flag_c();
-    }
-    log_pdp(TRACE, "%s", "add\n");
-}
-void do_nothing()
-{
-    log_pdp(TRACE, "%s", "unknown command\n");
-}
-void do_sob()
-{
-    reg[ss.adr] -= 1;
-    if (reg[ss.adr])
-    {
-        pc = pc - (dd.val * 2);
-        log_pdp(TRACE, "%s", "sob\n");
-    }
-    else
-        printf("\n");
-    return;
-}
-void do_movb()
-{
-    byte res = ss.val;
-    b_write(dd.adr, ss.val);
-    clear_flag_v();
-    if (res == 0)
-    {
-        set_flag_z();
-    }
-    else
-        clear_flag_z();
-    if ((res & 0200) == 0200)
-    {
-        set_flag_n();
-    }
-    else
-        clear_flag_n();
-
-    log_pdp(TRACE, "%s", "movb\n");
-}
-void do_clear()
-{
-    dd.val = 0;
-    log_pdp(TRACE, "%s", "clear\n");
-}
-void clear_all_flags()
-{
-    flag_C = 0;
-    flag_N = 0;
-    flag_V = 0;
-    flag_Z = 0;
-}
-void clear_flag_z()
-{
-    flag_Z = 0;
-}
-void clear_flag_c()
-{
-    flag_C = 0;
-}
-void clear_flag_v()
-{
-    flag_V = 0;
-}
-void clear_flag_n()
-{
-    flag_N = 0;
-}
-void set_all_flags()
-{
-    flag_C = 1;
-    flag_N = 1;
-    flag_V = 1;
-    flag_Z = 1;
-}
-void set_flag_z()
-{
-    flag_Z = 1;
-}
-void set_flag_c()
-{
-    flag_C = 1;
-}
-void set_flag_v()
-{
-    flag_V = 1;
-}
-void set_flag_n()
-{
-    flag_N = 1;
-}
-void do_br()
-{
-    pc = pc + XX * 2;
-    if (flag_Z != 1)
-        log_pdp(TRACE, "%s %06o\n", "br", pc);
-}
-void do_beq()
-{
-    log_pdp(TRACE, "%s %06o\n", "beq", pc + XX * 2);
-    if (flag_Z == 1)
-    {
-        do_br();
-    }
-}
-void do_testb()
-{
-    byte res = dd.val;
-    clear_flag_v();
-    clear_flag_c();
-    if (res == 0)
-    {
-        set_flag_z();
-    }
-    else
-        clear_flag_z();
-    if ((res & 0200) == 0200)
-    {
-        set_flag_n();
-    }
-    else
-        clear_flag_n();
-    log_pdp(TRACE, "%s\n", "testb");
-}
-void do_bpl()
-{
-    log_pdp(TRACE, "%s %06o\n", "bpl", pc + XX * 2);
-    if (flag_Z == 0)
-        do_br();
-}
-void do_jsr()
-{
-    sp -= 2;
-    w_write(sp, reg[registr]);
-    reg[registr] = pc;
-    pc = dd.adr;
-}
-void do_rts()
-{
-    pc = reg[registr];
-    reg[registr] = w_read(sp);
-    sp += 2;
 }
 Command cmd[] = {
     {0170000, 0060000, "add", do_add, HAS_SS | HAS_DD},
@@ -310,11 +114,11 @@ Command cmd[] = {
     {0170000, 0110000, "movb", do_movb, HAS_SS | HAS_DD},
     {0177700, 0005000, "clear", do_clear, HAS_DD},
     {0177700, 0001400, "beq", do_beq, HAS_XX},
-    {0177000, 0000000, "br", do_br, HAS_XX},
     {0177700, 0105700, "testb", do_testb, HAS_DD},
     {0177000, 0100000, "bpl", do_bpl, HAS_XX},
     {0177000, 0004000, "jsr", do_jsr, HAS_R | HAS_DD},
     {0177770, 0000200, "rts", do_rts, HAS_R},
+    {0177000, 0000000, "br", do_br, HAS_XX},
     {0, 0, "unknown", do_nothing, NO_PARAMS}};
 void run()
 {
